794f83433b54627c46cac6cf6830aa6d13a29c41
ifycode <marychord@gmail.com>
2021-01-18 08:04:26 -0800

    Routes continued - display on http://localhost:3000/orders

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
+    res.status(201).json({
         message: 'Handling GET requests to /products'
     });
 });
