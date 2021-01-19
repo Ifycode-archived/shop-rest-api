db4fada952bf6791e086c19c52171d33e9b5a140
ifycode <marychord@gmail.com>
2021-01-18 19:00:02 -0800

    Handling CORS

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Method', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
